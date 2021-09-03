9c375d0380086a533d3704c15d87a75eff37ac88
renevs <rene.vilar@jala-foundation.org>
2021-09-03 18:03:47 -0400

    solved.

--- RoundRobin.py
+++ RoundRobin.py
@@ -1,16 +1,17 @@
 def do_processing(job_cc, slice):
-    return (job_cc - slice, slice) if (job_cc - slice) >= 0 else (0, job_cc)
+    return slice if (job_cc - slice) >= 0 else job_cc
+
 
 def roundRobin(jobs, slice, index):
     current_job_index = 0
     total_cc = 0
     while jobs[index] > 0:
         if jobs[current_job_index] > 0:
-            remaining, processed = do_processing(jobs[current_job_index], slice)
+            processed = do_processing(jobs[current_job_index], slice)
             total_cc += processed
-            jobs[current_job_index] = remaining
-
+            jobs[current_job_index] -= processed
         current_job_index = (current_job_index + 1) if (current_job_index + 1) < len(jobs) else 0
     return total_cc
+
 
 print(roundRobin([16, 20, 15, 27, 27, 22, 20], 4, 3))
