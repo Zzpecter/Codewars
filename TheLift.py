a787952d5d08a54025bdf5379f1336eef8e2e4e3
renevs <rene.vilar@jala-foundation.org>
2021-09-08 07:03:19 -0400

    Solved.

--- TheLift.py
+++ TheLift.py
@@ -18,94 +18,120 @@ class Dinglemouse(object):
                         max_index = max(queue)
                     if index > max_index:
                         max_index = index
+            for index, queue in enumerate(self.current_users):
+                if len(queue) > 0:
+                    if max(queue) > max_index:
+                        max_index = max(queue)
             return max_index
 
         else:
             # search the lowest index
+            min_index = len(self.queues)
             for index, queue in enumerate(self.queues):
                 for person in queue:
                     if person < index:
-                        return person
-        return None
+                        min_index = person
+            if min(self.current_users) < min_index:
+                min_index = min(self.current_users)
+            return min_index
 
     def remove_duplicates(self, floor_list):
         seen = set()
         seen_add = seen.add
         return [x for x in floor_list if not (x in seen or seen_add(x))]
 
+    def check_queues(self):
+        for queue in self.queues:
+            if queue:
+                return True
+        return False
 
     def theLift(self):
-        output = [0]
-        target_floor = self.get_target_floor()
-        while target_floor is not None:
-            visited_floors = []
+        output = []
+        first = True
+
+        while self.check_queues():
+            if first:
+                visited_floors = [0]
+                first = False
+            else:
+                visited_floors = []
 
             if self.current_direction == 'UP':
-                for floor in range(self.current_floor, target_floor + 1):
+                for floor in range(self.current_floor, len(self.queues)):
                     stopped = False
 
                     # people get off first.
-                    if len(self.current_users) > 0:
-                        lift_user_index = len(self.current_users) - 1
-                        while lift_user_index >= 0:
-                            if self.current_users[lift_user_index] == floor:
-                                self.current_users.pop(lift_user_index)
-                                stopped = True
-                            lift_user_index -= 1
+                    pop_indexes = []
+                    for index, lift_user in enumerate(self.current_users):
+                        if lift_user == floor:
+                            pop_indexes.append(index)
+                            stopped = True
+
+                    for index in reversed(pop_indexes):
+                        self.current_users.pop(index)
 
                     # then they get in
-                    # todo I'm not respecting queue orders because I'm reading them from the back, need to be from front
-                    if len(self.queues[floor]) > 0:
-                        queue_index = len(self.queues[floor]) - 1
-                        while queue_index >= 0:
-                            if self.queues[floor][queue_index] > floor and len(self.current_users) < self.capacity:
-                                self.current_users.append(self.queues[floor][queue_index])
-                                self.queues[floor].pop(queue_index)
-                                stopped = True
+                    pop_indexes = []
+                    additional_people = 0
+                    for index, lift_user in enumerate(self.queues[floor]):
+                        if lift_user > floor:
+                            stopped = True
+                        if lift_user > floor and (len(self.current_users) + additional_people) < self.capacity:
+                            pop_indexes.append(index)
+                            additional_people += 1
+                            stopped = True
 
-                            queue_index -= 1
+                    for index in reversed(pop_indexes):
+                        self.current_users.append(self.queues[floor].pop(index))
 
                     if stopped:
                         visited_floors.append(floor)
             else:
-                for floor in range(self.current_floor, target_floor - 1, -1):
+                for floor in range(self.current_floor, -1, -1):
                     stopped = False
 
                     # people get off first.
-                    if len(self.current_users) > 0:
-                        lift_user_index = len(self.current_users) - 1
-                        while lift_user_index >= 0:
-                            if self.current_users[lift_user_index] == floor:
-                                self.current_users.pop(lift_user_index)
-                                stopped = True
-                            lift_user_index -= 1
+                    pop_indexes = []
+                    for index, lift_user in enumerate(self.current_users):
+                        if lift_user == floor:
+                            pop_indexes.append(index)
+                            stopped = True
+
+                    for index in reversed(pop_indexes):
+                        self.current_users.pop(index)
 
                     # then they get in
-                    if len(self.queues[floor]) > 0:
-                        queue_index = len(self.queues[floor]) - 1
-                        while queue_index >= 0:
-                            if self.queues[floor][queue_index] < floor and len(self.current_users) < self.capacity:
-                                self.current_users.append(self.queues[floor][queue_index])
-                                self.queues[floor].pop(queue_index)
-                                stopped = True
-                            queue_index -= 1
+                    pop_indexes = []
+                    additional_people = 0
+                    for index, lift_user in enumerate(self.queues[floor]):
+                        if lift_user < floor:
+                            stopped = True
+                        if lift_user < floor and (len(self.current_users) + additional_people) < self.capacity:
+                            pop_indexes.append(index)
+                            additional_people += 1
+
+                    for index in reversed(pop_indexes):
+                        self.current_users.append(self.queues[floor].pop(index))
 
                     if stopped:
                         visited_floors.append(floor)
-
-            output.extend(self.remove_duplicates(visited_floors))
+            no_dupes = self.remove_duplicates(visited_floors)
+            if output and no_dupes:
+                if output[len(output) - 1] == no_dupes[0]:
+                    no_dupes.pop(0)
+            output.extend(no_dupes)
 
             # after reaching target floor, change direction.
             self.current_floor = floor
             self.current_direction = 'DOWN' if self.current_direction == 'UP' else 'UP'
-            target_floor = self.get_target_floor()
 
-        if output[len(output) - 1] != 0:
+        if output and output[len(output) - 1] != 0:
             output.append(0)
-        return output
+        return output if output else [0]
 
 
-# Floors:    G     1      2        3     4      5      6         Answers:
+# #Floors:    G     1      2        3     4      5      6         Answers:
 # tests = [[((), (), (5, 5, 5), (), (), (), ()), [0, 2, 5, 0]],
 #          [((), (), (1, 1), (), (), (), ()), [0, 2, 1, 0]],
 #          [((), (3,), (4,), (), (5,), (), ()), [0, 1, 2, 3, 4, 5, 0]],
@@ -117,8 +143,8 @@ class Dinglemouse(object):
 #     print(a, answer)
 
 
-queues = ((2, 10, 2, 2), (4, 4), (), (8, 7, 4, 11), (3, 2, 0, 11, 3), (10, 3, 0, 10, 3), (3, 7, 2, 11, 9), (), (6, 7, 5), (3, 7, 1, 2, 2), (3, 7, 12, 0, 7), (9, 0), (6, 0, 0, 8))
-capacity = 3
+queues = ((), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))
+capacity = 5
 lift = Dinglemouse(queues, capacity)
 a = lift.theLift()
 print(a)
